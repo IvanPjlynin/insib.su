a2586cdfdb7871c9eab616a8926bc730fefd7e7f
IVAN POLUNIN <ipolynin@gmail.com>
2022-08-04 11:58:26 +0300

    first commit

--- github.php
+++ github.php
@@ -10,13 +10,13 @@ function execPrint($command) {
     print("</pre>");
 }
 // Print the exec output inside of a pre element
-<<<<<<< HEAD
+
 //execPrint("cd ~/var/www/6chester9/www/insib.su");
 
 execPrint("pwd");
-=======
+
 execPrint("cd /var/www/6chester9/www/insib.su");
->>>>>>> cd208430ff0ca433078abbc90cc642bd7028e479
+
 execPrint("git init");
 //////execPrint("git remote add origin git@github.com:IvanPjlynin/insib.git");
 execPrint("git pull https://github.com/IvanPjlynin/insib.su.git master");
